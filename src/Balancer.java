34db4cce7412616388f969f2870be03e4aa54d75
spolydor-tgm <spolydor@student.tgm.ac.at>
2016-02-26 11:48:53 +0100

    changes

--- src/Balancer.java
+++ src/Balancer.java
@@ -4,5 +4,31 @@ package src;
  * @author Stefan Polydor &lt;spolydor@student.tgm.ac.at&gt;
  * @version 26.02.16
  */
-public class Balancer {
+public class Balancer implements Runnable {
+	private int balanceMethod = 0;
+
+	public void balance() {
+
+	}
+
+	public void setBalanceMethod(int method) {
+		balanceMethod = method;
+	}
+
+
+	/**
+	 * When an object implementing interface <code>Runnable</code> is used
+	 * to create a thread, starting the thread causes the object's
+	 * <code>run</code> method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method <code>run</code> is that it may
+	 * take any action whatsoever.
+	 *
+	 * @see Thread#run()
+	 */
+	@Override
+	public void run() {
+		balance();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -4,5 +4,20 @@ package src;
  * @author Stefan Polydor &lt;spolydor@student.tgm.ac.at&gt;
  * @version 26.02.16
  */
-public class Server {
+public class Server implements Runnable {
+	/**
+	 * When an object implementing interface <code>Runnable</code> is used
+	 * to create a thread, starting the thread causes the object's
+	 * <code>run</code> method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method <code>run</code> is that it may
+	 * take any action whatsoever.
+	 *
+	 * @see Thread#run()
+	 */
+	@Override
+	public void run() {
+
+	}
 }
