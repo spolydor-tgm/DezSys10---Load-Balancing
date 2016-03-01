67817c0f102f9a71339893b1fdccfb3d7ed3fba4
spolydor-tgm <spolydor@student.tgm.ac.at>
2016-03-02 00:10:11 +0100

    edit balancer edited Server - should be finished

---
+++ src/src/Balancer.java
@@ -0,0 +1,97 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Stefan Polydor &lt;spolydor@student.tgm.ac.at&gt;
+ * @version 26.02.16
+ */
+public class Balancer implements Runnable {
+	/**
+	 * 0 ... Weighted Distribution
+	 * 1 ... Least Connection
+	 */
+	private int balanceMethod = 0;
+	private Map<Socket, Integer> servers = new HashMap<Socket, Integer>();
+	private ServerSocket serverSocket;
+	private boolean run = true;
+
+	public Balancer(){
+		
+	}
+
+	public Balancer(int balanceMethod, int port) {
+		this.balanceMethod = balanceMethod;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void balance() {
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+		Date date = new Date();
+		if (servers.isEmpty())
+			System.err.println(df.format(date) + "  : " + "Cannot Balance, No Server is connected");
+		else {
+			switch (balanceMethod) {
+				case 0:
+
+					break;
+				case 1:
+
+					break;
+			}
+		}
+	}
+
+	public void setBalanceMethod(int method) {
+		balanceMethod = method;
+	}
+
+	public void shutDown() {
+		run = false;
+	}
+
+	/**
+	 * When an object implementing interface <code>Runnable</code> is used
+	 * to create a thread, starting the thread causes the object's
+	 * <code>run</code> method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method <code>run</code> is that it may
+	 * take any action whatsoever.
+	 *
+	 * @see Thread#run()
+	 */
+	@Override
+	public void run() {
+		try {
+			String line;
+			while (run) {
+				Socket clientSocket = serverSocket.accept();
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+
+				while ((line = in.readLine()) != null) { //if
+					if (line.contains("SERVER"))
+						servers.put(clientSocket, servers.size()+1);
+					else
+						balance();
+				}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/src/Server.java
@@ -0,0 +1,92 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Stefan Polydor &lt;spolydor@student.tgm.ac.at&gt;
+ * @version 26.02.16
+ */
+public class Server implements Runnable {
+	private static int numbersForgiven = 0;
+	private int name = incrementNumbersForgiven();
+	private int port = 1234;
+	private String host = "localhost";
+	private BufferedReader in;
+	private boolean run = true;
+
+	private int incrementNumbersForgiven() {
+		Server.numbersForgiven+= 1;
+		return Server.numbersForgiven;
+	}
+
+	public Server() {}
+
+	public Server(int port) {
+		this.port = port;
+	}
+
+	public void setHost(String hostname) {
+		host = hostname;
+	}
+
+	public void connectToLB() {
+		try {
+			Socket clientSocket = new Socket(host, port);
+			in = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+		} catch (IOException e) {
+			System.err.println("Error during getting InputStreamReader");
+		}
+	}
+
+	/**
+	 * When an object implementing interface <code>Runnable</code> is used
+	 * to create a thread, starting the thread causes the object's
+	 * <code>run</code> method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method <code>run</code> is that it may
+	 * take any action whatsoever.
+	 *
+	 * @see Thread#run()
+	 */
+	@Override
+	public void run() {
+		connectToLB();
+		String line;
+		while (run) {
+			try {
+				while ((line = this.in.readLine()) != null) {
+					DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+					Date date = new Date();
+					System.out.println(df.format(date) + "  : " + line);
+				}
+			} catch (IOException ioe) {
+				System.err.println("Error occured");
+			}
+		}
+	}
+
+	public void shutDown() {
+		run = false;
+	}
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		Thread t = new Thread(server);
+		t.start();
+		System.out.println("Started");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Finished");
+	}
+}
